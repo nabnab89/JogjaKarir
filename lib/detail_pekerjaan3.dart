6e82f023e462a1612292c2f2b72e740880c01890
octaviyanna14 <opangestuti@gmail.com>
2021-07-07 17:04:47 +0700

    memperbaiki halaman detail_pekerjaan3 (class BerhasilMelamar)

--- lib/detail_pekerjaan3.dart
+++ lib/detail_pekerjaan3.dart
@@ -8,66 +8,60 @@ class BerhasilMelamar extends StatelessWidget {
       body: Container(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Padding(padding: EdgeInsets.only(top: 200),),
               Container(
-                height: MediaQuery.of(context).size.height * 0.17,
-                width: MediaQuery.of(context).size.height * 0.17,
-                decoration: BoxDecoration(
-                  color: Colors.blue[50],
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                ),
                 child: Center(
                   child: Image(
                     image: AssetImage("images/berhasil_lamaran.png"),
-                    height: MediaQuery.of(context).size.height * 0.09,
-                    width: MediaQuery.of(context).size.height * 0.09,
+                    height: MediaQuery.of(context).size.height * 0.18,
+                    width: MediaQuery.of(context).size.height * 0.18,
                   ),
                 ),
               ),
               Container(
                 margin: EdgeInsets.only(top: 24),
                 child: Text(
-                  "Cek email kamu",
+                  "Kamu Berhasil Melamar!",
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 25,
                     color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(top: 24, right: 24, left: 24, bottom: 24),
+                margin: EdgeInsets.only(right: 24, left: 24, bottom: 24),
                 child: Text(
-                  "Kami baru saja mengirimkan email untuk mengatur ulang kata sandi kamu",
+                  "Semoga Kamu Lolos ke Tahap Selanjutnya",
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 24,
+                    fontSize: 22,
                     fontWeight: FontWeight.normal,
                     color: Colors.black,
                   ),
                 ),
               ),
-              Padding(padding: EdgeInsets.only(top:30),),
-                    InkWell(
-                      child: Container(
-                        padding: EdgeInsets.symmetric(vertical: 15),
-                        width: 200,
-                        height: 45,
-                        child: Text(
-                          "Buka aplikasi email",
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                      ),
-                      onTap: () {},
-                    ),
+              Padding(padding: EdgeInsets.only(top:140),),
+              InkWell(
+                child: Container(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                margin: EdgeInsets.all(20),
+                width: double.infinity,
+                height: 45,
+                child: Text(
+                  "Kembali ke profil",
+                  style: TextStyle(
+                  color: Colors.white,
+                ),
+                  textAlign: TextAlign.center,
+                ),
+                  decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
