a76ce8fcff41e92ede8aeed334a1ebd93a1a1f93
gerin8 <85212495+gerin8@users.noreply.github.com>
2021-07-09 10:26:49 +0700

    Memperbaiki tampilan Register
    merubah icon dan menambahkan showhide password

--- lib/register.dart
+++ lib/register.dart
@@ -1,258 +1,249 @@
 import 'package:flutter/material.dart';
 import 'package:jogja_karir/login.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<Register> {
+  bool _isHidePassword = true;
+  bool _isHidePassword1 = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isHidePassword = !_isHidePassword;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          padding: EdgeInsets.all(20),
-          child: ListView(
-            children: <Widget>[
-              Center(
-                child: Column(
-                  children: <Widget>[
-                    _judul(),
-                    _textField(context),
-                    _button(context),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ) 
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Column(
+                children: <Widget>[
+                  _titleDescription(),
+                  _textField(),
+                  _buildButton(context),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Widget _judul() {
+  Widget _titleDescription() {
     return Column(
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.only(top: 5),
+          padding: EdgeInsets.only(top: 20.0),
         ),
         Text(
           "Register",
           style: TextStyle(
             color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            ),
+            fontSize: 30.0,
+          ),
         ),
         Padding(
-          padding: EdgeInsets.only(top: 50),
+          padding: EdgeInsets.only(top: 20.0),
         ),
         Text(
           "Buat Akun Baru",
           style: TextStyle(
+            fontSize: 40.0,
             color: Colors.blue,
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
           ),
-        textAlign: TextAlign.center,
+          textAlign: TextAlign.center,
         ),
       ],
     );
   }
 
-  Widget _textField(BuildContext context) {
+  Widget _textField() {
     return Column(
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.only(top: 50),
+          padding: EdgeInsets.only(top: 12.0),
         ),
         TextFormField(
           decoration: const InputDecoration(
-            icon: Icon(
-              Icons.alternate_email,
-              color: Colors.blue,
-            ),
             border: UnderlineInputBorder(),
             enabledBorder: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Colors.grey,
+                color: Color(0xff8b97ff),
                 width: 1.5,
               ),
             ),
             focusedBorder: UnderlineInputBorder(
               borderSide: BorderSide(
-                width: 3,
-                color: Colors.blue,
+                color: Colors.white,
+                width: 3.0,
               ),
             ),
-            hintText: "emailkamu@mail.com",
+            icon: Icon(Icons.alternate_email_rounded),
+            hintText: "emailkamu@gmail.com",
+            hintStyle: TextStyle(color: Color(0xffccd1ff)),
           ),
           style: TextStyle(color: Colors.black),
           autofocus: false,
         ),
         Padding(
-          padding: EdgeInsets.only(top: 12),
+          padding: EdgeInsets.only(top: 12.0),
         ),
         TextFormField(
-          decoration: const InputDecoration(
-            icon: Icon(
-              Icons.lock_outline,
-              color: Colors.blue,
-            ),
+          initialValue: '',
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
             border: UnderlineInputBorder(),
             enabledBorder: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Colors.grey,
+                color: Color(0xff8b97ff),
                 width: 1.5,
               ),
             ),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(
-                width: 3,
-                color: Colors.blue,
+            suffixIcon: GestureDetector(
+              onTap: () {
+                _togglePasswordVisibility();
+              },
+              child: Icon(
+                _isHidePassword ? Icons.visibility_off : Icons.visibility,
+                color: _isHidePassword ? Colors.grey : Colors.blue,
               ),
             ),
+            isDense: true,
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            icon: Icon(Icons.lock_rounded),
             hintText: "password kamu",
+            hintStyle: TextStyle(color: Color(0xffccd1ff)),
           ),
           style: TextStyle(color: Colors.black),
-          obscureText: true,
+          obscureText: _isHidePassword,
           autofocus: false,
         ),
         Padding(
-          padding: EdgeInsets.only(top: 12),
+          padding: EdgeInsets.only(top: 12.0),
         ),
         TextFormField(
-          decoration: const InputDecoration(
-            icon: Icon(
-              Icons.lock_outline,
-              color: Colors.blue,
-            ),
+          initialValue: '',
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
             border: UnderlineInputBorder(),
             enabledBorder: UnderlineInputBorder(
               borderSide: BorderSide(
-                color: Colors.grey,
+                color: Color(0xff8b97ff),
                 width: 1.5,
               ),
             ),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(
-                width: 3,
-                color: Colors.blue,
+            suffixIcon: GestureDetector(
+              onTap: () {
+                _togglePasswordVisibility();
+              },
+              child: Icon(
+                _isHidePassword1 ? Icons.visibility_off : Icons.visibility,
+                color: _isHidePassword1 ? Colors.grey : Colors.blue,
               ),
             ),
-            hintText: "ulangi password kamu",
+            isDense: true,
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+                width: 3.0,
+              ),
+            ),
+            icon: Icon(Icons.lock_rounded),
+            hintText: "ulangin password kamu",
+            hintStyle: TextStyle(color: Color(0xffccd1ff)),
           ),
           style: TextStyle(color: Colors.black),
-          obscureText: true,
+          obscureText: _isHidePassword1,
           autofocus: false,
         ),
       ],
     );
   }
 
-  Widget _button(BuildContext context) {
+  Widget _buildButton(BuildContext context) {
     return Column(
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.only(top: 15),
+          padding: EdgeInsets.only(top: 16.0),
         ),
         InkWell(
           child: Container(
-            padding: EdgeInsets.symmetric(vertical: 15),
+            padding: EdgeInsets.symmetric(vertical: 8.0),
             width: double.infinity,
             child: Text(
-              "Buat Akun",
-              style: TextStyle(
-                color: Colors.white,
-              ),
+              'Buat Akun',
+              style: TextStyle(color: Colors.white),
               textAlign: TextAlign.center,
             ),
             decoration: BoxDecoration(
               color: Colors.blue,
-              borderRadius: BorderRadius.circular(5),
+              borderRadius: BorderRadius.circular(10.0),
             ),
           ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 50),),
-            Text(
-              "Lupa Password ? ",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 12,
-                ),
-              textAlign: TextAlign.center,
-            ),
-            TextButton(
-              child: Text(
-                "Reset Password",
-                style: TextStyle(
-                  color: Colors.grey
-                ),
-              ),
-              onPressed: (){
-                Navigator.pushNamed(context, "/");
-              }
-            ),
-          ],
-        ),
         Padding(
-          padding: EdgeInsets.only(top: 50),
-        ),
-        Text(
-          "Atau",
-          style: TextStyle(
-            color: Colors.grey,
-            fontSize: 12,
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 20),
+          padding: EdgeInsets.only(top: 100),
         ),
         InkWell(
           child: Container(
             padding: EdgeInsets.symmetric(vertical: 15),
             width: double.infinity,
             child: Text(
-              "Register Dengan Google",
-              style: TextStyle(
-                color: Colors.white,
-              ),
+              'Registrasi Dengan Google',
+              style: TextStyle(color: Colors.white),
               textAlign: TextAlign.center,
             ),
             decoration: BoxDecoration(
               color: Colors.red,
-              borderRadius: BorderRadius.circular(5),
+              borderRadius: BorderRadius.circular(10.0),
             ),
           ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 20.0),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 60),),
+            Padding(
+              padding: EdgeInsets.only(top: 40),
+            ),
             Text(
-              "Sudah Memiliki Akun ? ",
+              "Sudah Punya Akun ? ",
               style: TextStyle(
                 color: Colors.black,
                 fontSize: 12,
-                ),
+              ),
               textAlign: TextAlign.center,
             ),
             TextButton(
               child: Text(
                 "Masuk",
-                style: TextStyle(
-                  color: Colors.blue,
-                ),
+                style: TextStyle(color: Colors.blue[400]),
               ),
-              onPressed: (){
+              onPressed: () {
+                // pindah ke halaman simple
                 Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Login()));
-              }
+                    MaterialPageRoute(builder: (context) => Login()));
+              },
             ),
           ],
         ),
       ],
     );
   }
-
 }
